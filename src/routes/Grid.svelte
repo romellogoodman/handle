35c03ca6cc1d80fc3375db085e828bc462b0b364
Romello Goodman <romellogood@gmail.com>
2024-02-29 20:29:34 -0500

    first pass at Grid and Menu

---
+++ src/routes/Grid.svelte
@@ -0,0 +1,26 @@
+<script>
+	let count = 6;
+
+	const items = new Array(count * count);
+</script>
+
+<main style="--grid: {3};">
+	{#each items as item}
+		<div class="item" />
+	{/each}
+</main>
+
+<style lang="scss">
+	main {
+		padding: 32px;
+		display: grid;
+		grid-template-columns: repeat(var(--grid), 1fr);
+		gap: 8px;
+		background-color: pink;
+	}
+
+	.item {
+		aspect-ratio: 1/1;
+		background-color: aqua;
+	}
+</style>
---
+++ src/routes/Menu.svelte
@@ -0,0 +1,67 @@
+<script>
+	let inputEl;
+	let isModalOpen = false;
+
+	const openModal = () => {
+		console.log('open modal');
+
+		isModalOpen = true;
+		inputEl.focus();
+	};
+
+	const closeModal = () => {
+		console.log('close modal');
+
+		isModalOpen = false;
+	};
+
+	function onKeyDown(event) {
+		console.log('keyCode', event.keyCode);
+
+		// 191 = /
+		if (event.keyCode === 191 && !isModalOpen) {
+			event.preventDefault();
+			openModal();
+		} else if (event.keyCode === 27) {
+			closeModal();
+		}
+	}
+</script>
+
+<svelte:window on:keydown={onKeyDown} />
+
+<header>
+	<div class="wrapper">
+		<input bind:this={inputEl} on:focus={openModal} on:blur={closeModal} type="text" />
+		{#if isModalOpen}
+			<div class="modal">
+				<p>modal</p>
+			</div>
+		{/if}
+	</div>
+</header>
+
+<style lang="scss">
+	header {
+		position: relative;
+		padding: 32px;
+		background-color: aquamarine;
+	}
+
+	.wrapper {
+		width: 100%;
+		position: relative;
+	}
+
+	input {
+		border: 1.5px solid #000000;
+		border-radius: 0px;
+		width: 100%;
+	}
+
+	.modal {
+		width: 100%;
+		background: papayawhip;
+		position: absolute;
+	}
+</style>

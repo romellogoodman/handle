5cb0442e9b098f9df9dec2ee2c047e2daa004963
Romello Goodman <romellogood@gmail.com>
2024-03-28 23:59:04 -0400

    keyboard and item interactions

--- src/routes/Menu.svelte
+++ src/routes/Menu.svelte
@@ -1,12 +1,13 @@
 <script>
-	let inputEl;
+	let inputRef;
+	let modalRef;
 	let isModalOpen = false;
 
 	const openModal = () => {
 		console.log('open modal');
 
 		isModalOpen = true;
-		inputEl.focus();
+		inputRef.focus();
 	};
 
 	const closeModal = () => {
@@ -24,18 +25,63 @@
 			openModal();
 		} else if (event.keyCode === 27) {
 			closeModal();
+		} else if (isModalOpen && event.keyCode === 40) {
+			console.log('down');
+			selectItem(true);
+		} else if (isModalOpen && event.keyCode === 38) {
+			console.log('up');
+			selectItem(false);
 		}
 	}
+
+	function selectItem(moveSelectDown) {
+		const selectedEl = modalRef.querySelector('.selected');
+
+		if (selectedEl) {
+			selectedEl.classList.remove('selected');
+
+			console.log('prev', selectedEl.previousElementSibling);
+			console.log('next', selectedEl.nextElementSibling);
+
+			if (moveSelectDown) {
+				if (selectedEl.nextElementSibling) {
+					// Select the next sibling
+					selectedEl.nextElementSibling.classList.add('selected');
+				} else {
+					// Loop back to the beginning
+					modalRef.firstChild.classList.add('selected');
+				}
+			} else {
+				if (selectedEl.previousElementSibling) {
+					// Select the prev sibling
+					selectedEl.previousElementSibling.classList.add('selected');
+				} else {
+					// Loop back to the end
+					modalRef.lastChild.classList.add('selected');
+				}
+			}
+		} else {
+			modalRef.firstChild.classList.add('selected');
+		}
+	}
+
+	const mouseOver = (ev) => {
+		ev.target.classList.add('selected');
+	};
 </script>
 
 <svelte:window on:keydown={onKeyDown} />
 
 <header>
 	<div class="wrapper">
-		<input bind:this={inputEl} on:focus={openModal} on:blur={closeModal} type="text" />
+		<input bind:this={inputRef} on:focus={openModal} on:blur={closeModal} type="text" />
 		{#if isModalOpen}
-			<div class="modal">
-				<p>modal</p>
+			<div bind:this={modalRef} class="modal">
+				<!-- <p onmouseover={mouseOver}>modal 1</p> -->
+				<p class="item">modal 1</p>
+				<p class="item">modal 2</p>
+				<p class="item">modal 3</p>
+				<p class="item">modal 4</p>
 			</div>
 		{/if}
 	</div>
@@ -61,7 +107,13 @@
 
 	.modal {
 		width: 100%;
+		height: 500px;
 		background: papayawhip;
 		position: absolute;
+
+		.item:hover,
+		:global(.selected) {
+			background-color: red;
+		}
 	}
 </style>
